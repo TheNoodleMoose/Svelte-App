b2704d12d3b3ec96713a5e356c88eb33355c134b
Christian Huffman <cjh605@gmail.com>
2019-09-29 20:21:05 -0400

    Adding Initial Files
    Welcome To  Svelte!

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let name;
+  import Hello from "./Components/Hello.svelte";
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<div>
+  <h1>Hello {name}!</h1>
+  <Hello />
+</div>
---
+++ src/Components/Hello.svelte
@@ -0,0 +1,19 @@
+<script>
+  let name = "world";
+  let count = 0;
+
+  const handleCount = () => {
+    count += 1;
+  };
+</script>
+
+<style>
+  .helloworld {
+    color: blue;
+  }
+</style>
+
+<div>
+  <p>You Have Clicked {count} {count === 1 ? 'time' : 'times'}!</p>
+  <button class="helloworld" on:click={handleCount}>Click Me!</button>
+</div>
